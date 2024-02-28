f4eac0dd00bcc58ad0ccb86a97a7d9567ea9a499
SushobhanLimbole <sushobhanlimbole17@gmail.com>
2024-02-29 00:55:42 +0530

    updated

--- codex/lib/pages/homepage.dart
+++ codex/lib/pages/homepage.dart
@@ -20,6 +20,7 @@ import 'package:codex/pages/tools/postman.dart';
 import 'package:codex/pages/tools/vscode.dart';
 import 'package:flutter/material.dart';
 import 'topics.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -46,12 +47,10 @@ class _HomePageState extends State<HomePage> {
   }
 
   Widget LanguageWidgets() {
-    return SingleChildScrollView(
-      scrollDirection: Axis.horizontal,
-      child: Container(
-        height: 210,
-        child: Row(
-          children: [
+    return Container(
+      height: 235,
+      child: CarouselSlider(
+          items: [
             Logo(
               "images/Java.jpeg",
               Syllabus(
@@ -59,82 +58,102 @@ class _HomePageState extends State<HomePage> {
                 code: javaCode,
                 titleName: "Java",
               ),
-              edgeInsetsMargin: EdgeInsets.only(left: 40),
+              // edgeInsetsMargin: EdgeInsets.only(left: 40),
             ),
             Logo("images/Dart-logo.png", DartSyllabus()),
             Logo("images/Python-logo.png", PythonSyllabus()),
             Logo(
               "images/js-logo.png",
               JsSyllabus(),
-              edgeInsetsMargin: EdgeInsets.only(left: 40, right: 40),
+              // edgeInsetsMargin: EdgeInsets.only(left: 40, right: 40),
             )
           ],
-        ),
-      ),
+          options: CarouselOptions(
+              enableInfiniteScroll: true,
+              autoPlay: true,
+              enlargeCenterPage: true,
+              autoPlayCurve: Curves.decelerate,
+              viewportFraction: 0.5,
+              enlargeFactor: 0.4,
+              initialPage: 0,
+              autoPlayAnimationDuration: Duration(seconds: 1),
+              autoPlayInterval: Duration(seconds: 2))),
     );
   }
 
   Widget FrameWorkWidgets() {
-    return SingleChildScrollView(
-      scrollDirection: Axis.horizontal,
-      child: Container(
-        height: 210,
-        child: Row(
-          children: [
-            Logo(
-              "images/flutter-logo.png",
-              FlutterSyllabus(),
-              edgeInsetsMargin: EdgeInsets.only(left: 40),
-            ),
-            Logo("images/spring-logo.png", SpringSyllabus()),
-            Logo("images/hibernate-logo.png", HibernateSyllabus()),
-            Logo(
-              "images/react-native-logo.png",
-              ReactNativeSyllabus(),
-              edgeInsetsMargin: EdgeInsets.only(left: 40, right: 40),
-            )
-          ],
-        ),
+    return Container(
+      height: 235,
+      child: CarouselSlider(
+        items: [
+          Logo(
+            "images/flutter-logo.png",
+            FlutterSyllabus(),
+            edgeInsetsMargin: EdgeInsets.only(),
+          ),
+          Logo("images/spring-logo.png", SpringSyllabus()),
+          Logo("images/hibernate-logo.png", HibernateSyllabus()),
+          Logo(
+            "images/react-native-logo.png",
+            ReactNativeSyllabus(),
+            edgeInsetsMargin: EdgeInsets.only(),
+          )
+        ],
+        options: CarouselOptions(
+            enableInfiniteScroll: true,
+            autoPlay: true,
+            enlargeCenterPage: true,
+            autoPlayCurve: Curves.decelerate,
+            viewportFraction: 0.5,
+            enlargeFactor: 0.4,
+            initialPage: 0,
+            reverse: true,
+            autoPlayAnimationDuration: Duration(seconds: 1),
+            autoPlayInterval: Duration(seconds: 2)),
       ),
     );
   }
 
   Widget DBWidgets() {
-    return SingleChildScrollView(
-      scrollDirection: Axis.horizontal,
-      child: Container(
-        height: 210,
-        child: Row(
-          children: [
+    return Container(
+        height: 235,
+        child: CarouselSlider(
+          items: [
             Logo(
               "images/mysql-img.png",
               MySQLSyllabus(),
-              edgeInsetsMargin: EdgeInsets.only(left: 40),
+              // edgeInsetsMargin: EdgeInsets.only(left: 40),
             ),
             Logo("images/mongodb.png", MongoDBSyllabus()),
             Logo("images/oracle.png", OracleSyllabus()),
             Logo(
               "images/maria.png",
               MariaDBSyllabus(),
-              edgeInsetsMargin: EdgeInsets.only(left: 40, right: 40),
+              // edgeInsetsMargin: EdgeInsets.only(left: 40, right: 40),
             )
           ],
-        ),
-      ),
-    );
+          options: CarouselOptions(
+              enableInfiniteScroll: true,
+              autoPlay: true,
+              enlargeCenterPage: true,
+              autoPlayCurve: Curves.decelerate,
+              viewportFraction: 0.5,
+              enlargeFactor: 0.4,
+              initialPage: 0,
+              autoPlayAnimationDuration: Duration(seconds: 1),
+              autoPlayInterval: Duration(seconds: 2)),
+        ));
   }
 
   Widget ToolsWidgets() {
-    return SingleChildScrollView(
-      scrollDirection: Axis.horizontal,
-      child: Container(
-        height: 210,
-        child: Row(
-          children: [
+    return Container(
+        height: 235,
+        child: CarouselSlider(
+          items: [
             Logo(
               "images/vs-logo.png",
               VSCodeSyllabus(),
-              edgeInsetsMargin: EdgeInsets.only(left: 40),
+              // edgeInsetsMargin: EdgeInsets.only(left: 40),
             ),
             Logo("images/eclipse.png", EclipseSyllabus()),
             Logo("images/git-img.png", GitSyllabus()),
@@ -142,40 +161,49 @@ class _HomePageState extends State<HomePage> {
             Logo(
               "images/postman.png",
               PostmanSyllabus(),
-              edgeInsetsMargin: EdgeInsets.only(left: 40, right: 40),
+              // edgeInsetsMargin: EdgeInsets.only(left: 40, right: 40),
             )
           ],
-        ),
-      ),
-    );
+          options: CarouselOptions(
+              enableInfiniteScroll: true,
+              autoPlay: true,
+              enlargeCenterPage: true,
+              autoPlayCurve: Curves.decelerate,
+              viewportFraction: 0.5,
+              enlargeFactor: 0.4,
+              initialPage: 0,
+              reverse: true,
+              autoPlayAnimationDuration: Duration(seconds: 1),
+              autoPlayInterval: Duration(seconds: 2)),
+        ));
   }
 
   Widget Logo(String logo, Widget routePage,
-      {EdgeInsets edgeInsetsMargin = const EdgeInsets.only(left: 40)}) {
+      {EdgeInsets edgeInsetsMargin = const EdgeInsets.only()}) {
     return InkWell(
       onTap: () {
         Navigator.push(
             context, MaterialPageRoute(builder: (context) => routePage));
       },
-      child: Container(
-        height: 130,
-        width: 130,
-        margin: edgeInsetsMargin,
-        decoration: BoxDecoration(
-            // border: Border.all(
-            //     color: Colors.black, width: 1, style: BorderStyle.solid),
-            boxShadow: [
-              BoxShadow(
-                  color: Colors.grey.shade600,
-                  offset: Offset(0, 13),
-                  spreadRadius: 3,
-                  blurRadius: 16),
-            ],
-            borderRadius: BorderRadius.circular(20),
-            image: DecorationImage(
-              image: AssetImage("$logo"),
-              fit: BoxFit.cover,
-            )),
+      child: Center(
+        child: Container(
+          height: 150,
+          width: 150,
+          margin: edgeInsetsMargin,
+          decoration: BoxDecoration(
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.grey.shade600,
+                    offset: Offset(0, 15),
+                    spreadRadius: 4,
+                    blurRadius: 16),
+              ],
+              borderRadius: BorderRadius.circular(20),
+              image: DecorationImage(
+                image: AssetImage("$logo"),
+                fit: BoxFit.cover,
+              )),
+        ),
       ),
     );
   }
