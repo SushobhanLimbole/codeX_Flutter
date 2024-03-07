a00457e678a5969c38d1ff48d8b825388689cba0
SushobhanLimbole <sushobhanlimbole17@gmail.com>
2024-03-08 00:05:07 +0530

    updated

--- new_codex_app/lib/pages/homepage.dart
+++ new_codex_app/lib/pages/homepage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:new_codex_app/pages/topicsscreen.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({super.key});
@@ -22,44 +23,143 @@ class _MyHomePageState extends State<HomePage> {
   }
 
   Widget coursesCard({String? text, String? img}) {
-    return Container(
-      margin: EdgeInsets.only(left: 20),
-      height: 180,
-      width: 150,
-      decoration: BoxDecoration(
-          gradient: LinearGradient(
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-              colors: [
-                Color.fromRGBO(124, 132, 233, 0.8),
-                Color.fromRGBO(219, 19, 19, 0.8)
-              ]),
-          boxShadow: [
-            BoxShadow(
-                offset: Offset(0, 4),
-                blurRadius: 4,
-                color: Color.fromRGBO(0, 0, 0, 0.25))
-          ],
-          borderRadius: BorderRadius.circular(10)),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Container(
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => TopicsScreen(),
+            ));
+      },
+      child: Container(
+        margin: EdgeInsets.only(left: 20),
+        height: 180,
+        width: 150,
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+                  Color.fromRGBO(124, 132, 233, 0.8),
+                  Color.fromRGBO(219, 19, 19, 0.8)
+                ]),
+            boxShadow: [
+              BoxShadow(
+                  offset: Offset(0, 4),
+                  blurRadius: 4,
+                  color: Color.fromRGBO(0, 0, 0, 0.25))
+            ],
+            borderRadius: BorderRadius.circular(10)),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Container(
+                width: 140,
+                child: Center(
+                  child: Text(
+                    "$text",
+                    style: GoogleFonts.anekTamil(
+                        color: Colors.white,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500),
+                  ),
+                )),
+            Container(
               width: 140,
-              child: Center(
-                child: Text(
-                  "$text",
-                  style: GoogleFonts.anekTamil(
-                      color: Colors.white,
-                      fontSize: 16,
-                      fontWeight: FontWeight.w500),
-                ),
-              )),
-          Container(
-            width: 140,
-            child: Image.asset("$img"),
-          )
-        ],
+              child: Image.asset("$img"),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget upcomingCoursesCard() {
+    return InkWell(
+      child: Center(
+        child: Container(
+          height: 170,
+          width: MediaQuery.of(context).size.width - 40,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+                  Color.fromRGBO(242, 141, 141, 1),
+                  Color.fromRGBO(98, 21, 21, 1)
+                ]),
+            borderRadius: BorderRadius.circular(15),
+          ),
+          child: Center(
+            child: Container(
+              width: MediaQuery.of(context).size.width - 60,
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                            margin: EdgeInsets.only(top: 10, left: 5),
+                            child: Text(
+                              '9th March 2024',
+                              style: GoogleFonts.anekTamil(
+                                  color: Colors.white,
+                                  fontSize: 22,
+                                  fontWeight: FontWeight.w500),
+                            ),
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(top: 10, left: 5),
+                            child: Text(
+                              '10am - 12am',
+                              style: GoogleFonts.anekTamil(
+                                color: Colors.white,
+                                fontSize: 16,
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                      SizedBox(
+                        width: 50,
+                      ),
+                      InkWell(
+                        child: Container(
+                          height: 30,
+                          width: 80,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              color: Color.fromRGBO(43, 137, 201, 1)),
+                          child: Center(
+                              child: Text(
+                            'Join Demo',
+                            style: GoogleFonts.anekTamil(color: Colors.white),
+                          )),
+                        ),
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    child: Center(
+                      child: Text(
+                        'Flutter app Development',
+                        style: GoogleFonts.anekTamil(
+                            fontSize: 23,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w500),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
@@ -124,8 +224,50 @@ class _MyHomePageState extends State<HomePage> {
             children: [
               coursesCard(
                   text: "Java Full Stack", img: "images/java_full_stack.png"),
-              coursesCard(text: "MERN Stack", img: "images/java_full_stack.png")
+              Container(
+                margin: EdgeInsets.only(left: 20),
+                height: 180,
+                width: 150,
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        colors: [
+                          Color.fromRGBO(244, 132, 132, 1),
+                          Color.fromRGBO(153, 4, 4, 1)
+                        ]),
+                    boxShadow: [
+                      BoxShadow(
+                          offset: Offset(0, 4),
+                          blurRadius: 4,
+                          color: Color.fromRGBO(0, 0, 0, 0.25))
+                    ],
+                    borderRadius: BorderRadius.circular(10)),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Container(
+                        width: 140,
+                        child: Center(
+                          child: Text(
+                            "MERN Stack",
+                            style: GoogleFonts.anekTamil(
+                                fontSize: 16, fontWeight: FontWeight.w500),
+                          ),
+                        )),
+                    Container(
+                      width: 140,
+                      child: Image.asset("images/mern-stack.png"),
+                    )
+                  ],
+                ),
+              ),
             ],
+          ),
+          title(text: 'Upcoming Courses'),
+          upcomingCoursesCard(),
+          SizedBox(
+            height: 50,
           )
         ],
       ),
---
+++ new_codex_app/lib/pages/topicsscreen.dart
@@ -0,0 +1,157 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:new_codex_app/pages/homepage.dart';
+
+class TopicsScreen extends StatefulWidget {
+  TopicsScreen({super.key});
+
+  @override
+  State<TopicsScreen> createState() => _TopicsScreenState();
+}
+
+class _TopicsScreenState extends State<TopicsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+          height: double.infinity,
+          width: MediaQuery.of(context).size.width,
+          decoration: BoxDecoration(
+              // color: Color.fromRGBO(44, 55, 149, 0.67),
+              gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                Color.fromRGBO(124, 132, 233, 0.8),
+                Color.fromRGBO(219, 19, 19, 0.8)
+              ])),
+          child: SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.only(top: 40, left: 5),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          IconButton(
+                            onPressed: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => HomePage(),
+                                  ));
+                            },
+                            icon: Icon(
+                              Icons.arrow_back_ios_new_sharp,
+                              color: Colors.white,
+                            ),
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(left: 30),
+                            child: Text('Java',
+                                style: GoogleFonts.jost(
+                                    color: Color.fromRGBO(255, 252, 252, 1),
+                                    fontSize: 45,
+                                    fontWeight: FontWeight.w500)),
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(left: 30),
+                            child: Text('Full Stack',
+                                style: GoogleFonts.quicksand(
+                                    color: Color.fromRGBO(255, 252, 252, 1),
+                                    fontSize: 30,
+                                    fontWeight: FontWeight.w600)),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          )
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(top: 70),
+                      height: 140,
+                      width: 140,
+                      decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          gradient: LinearGradient(
+                              begin: Alignment.topCenter,
+                              end: Alignment.bottomCenter,
+                              colors: [
+                                // Color.fromRGBO(194, 255, 197, 1),
+                                Color.fromRGBO(62, 35, 232, 1),
+                                Color.fromRGBO(0, 0, 0, 1)
+                              ])),
+                      child: Image.asset('images/java.png'),
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  height: 550,
+                  decoration: BoxDecoration(
+                      color: Color.fromRGBO(255, 255, 255, 1),
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(40),
+                          topRight: Radius.circular(40))),
+                  margin: EdgeInsets.only(top: 20),
+                  child: ListView.builder(
+                    itemCount: 5,
+                    itemBuilder: (context, index) {
+                      return Center(
+                        child: Container(
+                          margin: EdgeInsets.only(top: 20),
+                          decoration: BoxDecoration(
+                              boxShadow: [
+                                BoxShadow(
+                                    offset: Offset(2, 4),
+                                    blurRadius: 4,
+                                    color: Color.fromRGBO(0, 0, 0, 0.35))
+                              ],
+                              color: Colors.white,
+                              borderRadius: BorderRadius.only(
+                                  bottomRight: Radius.circular(10),
+                                  topRight: Radius.circular(10),
+                                  bottomLeft: Radius.circular(55),
+                                  topLeft: Radius.circular(55))),
+                          height: 110,
+                          width: MediaQuery.of(context).size.width - 30,
+                          child: Row(
+                            children: [
+                              Container(
+                                margin: EdgeInsets.only(left: 10),
+                                height: 80,
+                                width: 90,
+                                decoration: BoxDecoration(
+                                    color: Colors.black54,
+                                    shape: BoxShape.circle),
+                              ),
+                              Container(
+                                margin: EdgeInsets.only(left: 10),
+                                child: Text(
+                                  'Introduction to Java',
+                                  style: TextStyle(fontSize: 16),
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                )
+              ],
+            ),
+          )),
+    );
+  }
+}
